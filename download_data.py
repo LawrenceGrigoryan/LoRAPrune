f4940b857007644b4af8418d9a63d48b0b252e72
grigorianl <grigorianl@master.ismll.de>
2026-03-02 18:18:00 +0100

    add cluster running script, data loading script, adjust requirements

---
+++ download_data.py
@@ -0,0 +1,3 @@
+from datasets import load_dataset
+
+data = load_dataset(path="MBZUAI/LaMini-instruction", trust_remote_code=True)
---
+++ script/prune_cluster.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=loraprune_test
+#SBATCH --output=loraprune_test%j.log
+#SBATCH --error=loraprune_test%j.err
+#SBATCH --mail-user=REPLACE_USER_NAME@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+srun python prune.py \
+    --base_model "meta-llama/Llama-3.2-1B" \
+    --data_path 'MBZUAI/LaMini-instruction' \
+    --output_dir 'outputs_dir' \
+    --batch_size 16 \
+    --micro_batch_size 2 \
+    --num_epochs 2 \
+    --learning_rate 1e-4 \
+    --cutoff_len 512 \
+    --val_set_size 1000 \
+    --lora_r 8 \
+    --lora_alpha 16 \
+    --lora_dropout 0.05 \
+    --lora_target_modules '[q_proj, k_proj, v_proj, o_proj, gate_proj,up_proj, down_proj]' \
+    --train_on_inputs \
+    --group_by_length \
+    --ratio 0.5 \
+    --prune_metric 'lora' \
+    --prune_freq 10 \
