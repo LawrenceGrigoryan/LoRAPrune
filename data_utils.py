07dbdd1e68aa3fd3295767751749a40159b44160
LawrenceGrigoryan <glavrentiy123@gmail.com>
2026-03-05 22:09:07 +0100

    fix the usage of bos token for sft

--- data_utils.py
+++ data_utils.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer
 
 
-def tokenize(prompt: str, tokenizer: AutoTokenizer, cutoff_len: int, add_eos_token: bool = True):
+def tokenize(prompt: str, tokenizer: AutoTokenizer, cutoff_len: int, add_eos_token: bool = True, **kwargs):
     # there's probably a way to do this with the tokenizer settings
     # but again, gotta move fast
     result = tokenizer(
@@ -10,6 +10,7 @@ def tokenize(prompt: str, tokenizer: AutoTokenizer, cutoff_len: int, add_eos_tok
         max_length=cutoff_len-1,  # reserve space for EOS token
         padding=False,
         return_tensors=None,
+        **kwargs,
     )
     if (
         result["input_ids"][-1] != tokenizer.eos_token_id
@@ -35,18 +36,19 @@ def generate_sft_sample(data_point):
 {data_point["response"]}"""
 
 
-def generate_and_tokenize_prompt(data_point: dict, train_on_inputs: bool):
-    full_prompt = data_point["text"]
-    tokenized_full_prompt = tokenize(full_prompt)
+def generate_and_tokenize_prompt(data_point: dict, tokenizer: AutoTokenizer,cutoff_len: int, train_on_inputs: bool):
     if not train_on_inputs:
+        tokenized_full_prompt = tokenize(generate_sft_sample(data_point), tokenizer, cutoff_len=cutoff_len-1, add_eos_token=True, add_bos_token=False)
         user_prompt = generate_sft_sample({**data_point, "response": ""})
-        tokenized_user_prompt = tokenize(user_prompt, add_eos_token=False)
+        tokenized_user_prompt = tokenize(user_prompt, tokenizer, cutoff_len=cutoff_len, add_eos_token=False, add_bos_token=False)
         user_prompt_len = len(tokenized_user_prompt["input_ids"])
 
         tokenized_full_prompt["labels"] = [
             -100
-        ] * user_prompt_len + tokenized_full_prompt["labels"][
+        ] * user_prompt_len + [tokenizer.bos_token_id] + tokenized_full_prompt["labels"][
             user_prompt_len:
         ]  # could be sped up, probably
-
+    else:
+        full_prompt = data_point["text"]
+        tokenized_full_prompt = tokenize(full_prompt, tokenizer, cutoff_len=cutoff_len, add_eos_token=True)
     return tokenized_full_prompt
