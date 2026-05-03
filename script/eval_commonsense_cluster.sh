caf41bc72456e108de7e7eee195b1a4470ecc314
Lavrentii Grigorian <glavrentiy123@gmail.com>
2026-05-03 14:36:08 +0200

    upd: better default eval commonsense script

--- script/eval_commonsense_cluster.sh
+++ script/eval_commonsense_cluster.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-#SBATCH --job-name=loraprune_qwen1.5-0.5b-mmlu
-#SBATCH --output=./logs/loraprune_qwen1.5-0.5b-mmlu%j.log
-#SBATCH --error=./logs/loraprune_qwen1.5-0.5b-mmlu%j.err
+#SBATCH --job-name=loraprune_qwen1.5-0.5b-eval-commonsense
+#SBATCH --output=./logs/loraprune_qwen1.5-0.5b-eval-commonsense-%j.log
+#SBATCH --error=./logs/loraprune_qwen1.5-0.5b-eval-commonsense-%j.err
 #SBATCH --mail-user=REPLACE_USER_NAME@uni-hildesheim.de
 #SBATCH --partition=STUD
 #SBATCH --gres=gpu:1
@@ -10,8 +10,8 @@ export HF_DATASETS_OFFLINE=1
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_CACHE="./data/benchmarks/"
 
-srun python ./evaluation/eval_commonsense.py \
+srun python eval_commonsense.py \
     --model_id "./models/Qwen_Qwen1.5-0.5B" \
-    # --adapter_id "./outputs_dir/qwen15-05b-c4-100k/" \
+    # --adapter_id "./outputs_dir/qwen15-05b-c4-80k/" \
     --batch_size 8 \
     --output_dir "../output_dir/evaluation/results/"
