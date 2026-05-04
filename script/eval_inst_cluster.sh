a961d8d49d292325b2afe504905b62265821ed9d
Lavrentii Grigorian <glavrentiy123@gmail.com>
2026-05-04 20:33:17 +0200

    add: sh script to run instruction following eval

---
+++ script/eval_inst_cluster.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#SBATCH --job-name=loraprune_qwen1.5-0.5b-eval-instruction
+#SBATCH --output=./logs/loraprune_qwen1.5-0.5b-eval-instruction-%j.log
+#SBATCH --error=./logs/loraprune_qwen1.5-0.5b-eval-instruction-%j.err
+#SBATCH --mail-user=REPLACE_USER_NAME@uni-hildesheim.de
+#SBATCH --partition=STUD
+#SBATCH --gres=gpu:1
+
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+export HF_DATASETS_CACHE="./data/benchmarks/"
+
+srun python eval_instruction.py \
+    --model_id "./models/Qwen_Qwen1.5-0.5B-Chat" \
+    # --adapter_id "./outputs_dir/qwen15-05b-c4-80k/" \
+    --batch_size 8 \
+    --output_dir "../output_dir/evaluation/results/"
