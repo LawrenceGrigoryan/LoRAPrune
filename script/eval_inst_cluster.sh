3be3db8344759b758ca8cded65d5bbad19edafd1
Lavrentii Grigorian <glavrentiy123@gmail.com>
2026-05-14 18:46:31 +0200

    eval sh eval scripts (output_dir)

--- script/eval_inst_cluster.sh
+++ script/eval_inst_cluster.sh
@@ -14,4 +14,4 @@ srun python eval_instruction.py \
     --model_id "./models/Qwen_Qwen1.5-0.5B-Chat" \
     --adapter_id "./outputs_dir/qwen15-05b-chat-lamini-100k/" \
     --batch_size 8 \
-    --output_dir "./output_dir/evaluation/results/"
+    --output_dir "./outputs_dir/evaluation/results/"
