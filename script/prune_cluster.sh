527297906d651617d4cc2037ae79c0878a506fa4
Lavrentii Grigorian <glavrentiy123@gmail.com>
2026-07-12 16:48:15 +0200

    fix booleans in script

--- script/prune_cluster.sh
+++ script/prune_cluster.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=loraprune_test
-#SBATCH --output=./logs/loraprune_qwen15-05b-c4-20k_%j.log
-#SBATCH --error=./logs/loraprune_qwen15-05b-c4-20k_%j.err
+#SBATCH --output=./logs/loraprune_llama3-coupled-gqa-test_%j.log
+#SBATCH --error=./logs/loraprune_llama3-coupled-gqa-test_%j.err
 #SBATCH --mail-user=REPLACE_USER_NAME@uni-hildesheim.de
 #SBATCH --partition=STUD
 #SBATCH --gres=gpu:1
 
 srun python prune.py \
-    --base_model "./models/Qwen_Qwen1.5-0.5B" \
+    --base_model "./models/meta_llama_Llama-3.2-1B" \
     --data_path './data/allenai___c4_120k' \
-    --output_dir 'outputs_dir/qwen15-05b-c4-20k-ratio-0.5' \
+    --output_dir 'outputs_dir/llama3-coupled-gqa-test' \
     --num_epochs 2 \
     --batch_size 128 \
     --micro_batch_size 1 \
@@ -17,13 +17,15 @@ srun python prune.py \
     --cutoff_len 512 \
     --train_set_size 20000 \
     --val_set_size 1000 \
-    --lora_r 16 \
-    --lora_alpha 32 \
+    --lora_r 8 \
+    --lora_alpha 16 \
     --lora_dropout 0.05 \
     --lora_target_modules '[q_proj, k_proj, v_proj, o_proj, gate_proj,up_proj, down_proj]' \
     --train_on_inputs \
     --group_by_length \
-    --ratio 0.5 \
+    --ratio 0.2 \
     --prune_metric 'lora' \
     --prune_freq 10 \
-    --fp16 false
+    --fp16 False \
+    --adaptive_ema False \
+
